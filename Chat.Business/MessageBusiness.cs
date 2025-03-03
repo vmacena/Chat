23d7b0a362d5b01e6e6cd718257c5fe2da8d5d39
vmacena <macena.v@aluno.ifsp.edu.br>
2025-03-03 11:54:01 -0300

    feat: implement MessageBusiness for retrieving messages from the database

---
+++ Chat.Business/MessageBusiness.cs
@@ -0,0 +1,21 @@
+﻿using Chat.Core.Persistence;
+
+namespace Chat.Business
+{
+    public class MessageBusiness(ChatDbContext dbContext)
+    {
+        private readonly ChatDbContext _dbContext = dbContext;
+
+        public IQueryable<object> GetAllMessages()
+        {
+            return _dbContext
+                .Messages.OrderBy(m => m.Timestamp)
+                .Select(m => new
+                {
+                    user = m.User ?? "",
+                    content = m.Content ?? "",
+                    timestamp = m.Timestamp,
+                });
+        }
+    }
+}
