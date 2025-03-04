81b585278ca14a9e9cdee186f35a9a9ab1bbfcfc
vmacena <macena.v@aluno.ifsp.edu.br>
2025-03-04 12:59:30 -0300

    feat: configure dbcontext to use npgsql for database connection

--- src/Chat.API/Program.cs
+++ src/Chat.API/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ChatDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddStartupServices();
 
