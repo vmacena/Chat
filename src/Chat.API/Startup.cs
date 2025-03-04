47aa2dce3bc3c0fd2245a01d6e0478424fcf6b79
vmacena <macena.v@aluno.ifsp.edu.br>
2025-03-04 10:38:16 -0300

    feat: refactor program and add startup for service configuration

---
+++ src/Chat.API/Startup.cs
@@ -0,0 +1,59 @@
+public static class StartupExtensions
+{
+    public static void AddStartupServices(this IServiceCollection services)
+    {
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void UseStartupMiddleware(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        var summaries = new[]
+        {
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching",
+        };
+
+        app.MapGet(
+                "/weatherforecast",
+                () =>
+                {
+                    var forecast = Enumerable
+                        .Range(1, 5)
+                        .Select(index => new WeatherForecast(
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            summaries[Random.Shared.Next(summaries.Length)]
+                        ))
+                        .ToArray();
+                    return forecast;
+                }
+            )
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
