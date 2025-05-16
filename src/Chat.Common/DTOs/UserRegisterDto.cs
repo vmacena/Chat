753a9692a03bb20669477a5b8ef227f96aa959e1
vmacena <macena.v@aluno.ifsp.edu.br>
2025-05-16 10:15:19 -0300

    feat: add user register dto

---
+++ src/Chat.Common/DTOs/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace Chat.Common.DTOs
+{
+    public class UserRegisterDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
