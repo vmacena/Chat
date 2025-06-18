11970b263f63bcc5f7a7a3dabb221fbbf16058e6
vmacena <macena.v@aluno.ifsp.edu.br>
2025-06-18 01:06:02 -0300

    refactor: remove unused database context and clean up program file

--- src/Chat.API/Program.cs
+++ src/Chat.API/Program.cs
@@ -1,16 +1,7 @@
-using Chat.Core.Persistence;
-using Microsoft.EntityFrameworkCore;
+using Chat.API;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<ChatDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
 builder.Services.AddStartupServices(builder.Configuration);
-
 var app = builder.Build();
-
 app.UseStartupMiddleware();
-
 app.Run();
